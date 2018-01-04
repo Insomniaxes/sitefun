69f0979e90dffd26c798029c00221421ee1c70b3
Insomniaxes <stef@sitefun.be>
2018-01-04 16:29:41 +0100

    eerste test

---
+++ src/main/java/be/sitefun/WebApplication.java
@@ -0,0 +1,13 @@
+package be.sitefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "be.sitefun")
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
